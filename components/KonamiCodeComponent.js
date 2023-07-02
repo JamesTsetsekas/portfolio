f8b42648a9920c6ffbe519bde6eb26097d40f33c
James Tsetsekas <jtsetsekas@gmail.com>
2023-07-02 02:07:04 -0400

    Update and rename Konami.js to KonamiCodeComponent.js

--- components/KonamiCodeComponent.js
+++ components/KonamiCodeComponent.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
-import Confetti from "react-confetti";
+import React, { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 
 const KonamiCodeComponent = () => {
-  const [konamiCode, setKonamiCode] = useState("");
-  const konamiPattern =
-    "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter";
+  const [konamiCode, setKonamiCode] = useState('');
+  const konamiPattern = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter';
   const [showConfetti, setShowConfetti] = useState(false);
 
   const handleKeyDown = (event) => {
@@ -18,21 +17,21 @@ const KonamiCodeComponent = () => {
         setShowConfetti(true);
         setTimeout(() => {
           setShowConfetti(false);
-        }, 10000); // Show confetti for 3 seconds
+        }, 3000); // Show confetti for 3 seconds
       }
     };
 
     if (konamiCode.length >= konamiPattern.length) {
-      setKonamiCode("");
+      setKonamiCode('');
     }
 
     checkKonamiCode();
   }, [konamiCode]);
 
   useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
     return () => {
-      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
