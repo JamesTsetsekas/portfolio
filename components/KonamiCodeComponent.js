5fc7bb95afa900957bb81adfac93ec42e336fa3b
James Tsetsekas <jtsetsekas@gmail.com>
2023-07-02 02:15:22 -0400

    Update KonamiCodeComponent.js

--- components/KonamiCodeComponent.js
+++ components/KonamiCodeComponent.js
@@ -42,4 +42,6 @@ const KonamiCodeComponent = () => {
   );
 };
 
+console.log('Wow, look at you checking the console you must be a 1337 H4x0r! For fun try entering the Konami Code on my page! ↑ ↑ ↓ ↓ ← → ← → b a Enter on your keyboard.')
+
 export default KonamiCodeComponent;
