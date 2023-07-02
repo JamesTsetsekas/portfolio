4a42904eb37260c4d220cdb2d5263c180e9bd102
James Tsetsekas <jtsetsekas@gmail.com>
2023-07-02 01:32:47 -0400

    Create Konami.js

---
+++ components/Konami.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+
+const KonamiCodeComponent = () => {
+  const [konamiCode, setKonamiCode] = useState("");
+  const konamiPattern =
+    "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter";
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  const handleKeyDown = (event) => {
+    const { key } = event;
+    setKonamiCode((prevCode) => prevCode + key);
+  };
+
+  useEffect(() => {
+    const checkKonamiCode = () => {
+      if (konamiCode === konamiPattern) {
+        setShowConfetti(true);
+        setTimeout(() => {
+          setShowConfetti(false);
+        }, 10000); // Show confetti for 3 seconds
+      }
+    };
+
+    if (konamiCode.length >= konamiPattern.length) {
+      setKonamiCode("");
+    }
+
+    checkKonamiCode();
+  }, [konamiCode]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div>
+      {showConfetti && <Confetti />}
+    </div>
+  );
+};
+
+export default KonamiCodeComponent;
