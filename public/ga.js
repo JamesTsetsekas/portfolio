8ee735230565c6ed44ba804d928d4d336c1c3ed4
James Tsetsekas <jtsetsekas@gmail.com>
2023-12-11 13:15:49 -0500

    Update ga.js

--- public/ga.js
+++ public/ga.js
@@ -26,3 +26,17 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-3DEGVN62JJ');
+
+
+// Clarity
+// clarity.js
+(function(c, l, a, r, i, t, y) {
+  c[a] = c[a] || function() {
+    (c[a].q = c[a].q || []).push(arguments)
+  };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "k4axbvd7w1");
