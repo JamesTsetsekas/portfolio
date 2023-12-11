4634fca1d6af6499264d9639b77e1d37288d81dc
James Tsetsekas <jtsetsekas@gmail.com>
2023-12-11 09:06:28 -0500

    Update ga.js new account tags, + analytics

--- public/ga.js
+++ public/ga.js
@@ -1,4 +1,28 @@
+// Google Tag Manager
+(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-TNT5TB5V');
+
+// Google Analytics tag (gtag.js)
+(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtag.js' });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'G-3DEGVN62JJ');
+
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() {
+    dataLayer.push(arguments);
+}
 gtag('js', new Date());
 gtag('config', 'G-3DEGVN62JJ');
