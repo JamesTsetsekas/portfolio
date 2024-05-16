6d146f3bbf840bc269ea7570d6ed62281c6e0c41
James Tsetsekas <jtsetsekas@gmail.com>
2024-05-16 11:19:48 -0400

    use bitpay instead of coingecko for bitcoin price check

--- components/Konami.js
+++ components/Konami.js
@@ -24,9 +24,10 @@ const Konami = () => {
 
   const fetchBitcoinPrice = async () => {
     try {
-      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+      const response = await fetch('https://bitpay.com/rates');
       const data = await response.json();
-      const price = data.bitcoin.usd;
+      const usdRate = data.data.find(currency => currency.code === 'USD');
+      const price = usdRate.rate;
       setBitcoinPrice(price);
 
       if (price > 100000 && !showBitcoinConfetti) {
