6e2c2ee0da67376ae3e386bb8c522cf7b90842c1
JamesTsetsekas <jtsetsekas@gmail.com>
2024-12-27 03:10:33 +0000

    Deploying to gh-pages from @ JamesTsetsekas/portfolio@af60e1bd9174ffa8213ca9ca54c153294ee55a4d 🚀

--- ga.js
+++ ga.js
@@ -1,4 +1,40 @@
+// Google Tag Manager
+(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-TNT5TB5V');
+
+// Google Analytics tag (gtag.js)
+(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtag.js' });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'G-3DEGVN62JJ');
+
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() {
+    dataLayer.push(arguments);
+}
 gtag('js', new Date());
-gtag('config', 'G-DDRHB0Q9H2');
+gtag('config', 'G-3DEGVN62JJ');
+
+// Clarity
+(function(c, l, a, r, i, t, y) {
+  c[a] = c[a] || function() {
+    (c[a].q = c[a].q || []).push(arguments)
+  };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "k4axbvd7w1");
